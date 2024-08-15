54049edcbe2ce34bbf8bb64c4064992af2b497b1
Steep Salvador <stevesalvadorman@gmail.com>
2024-08-15 16:30:36 -0500

    Avance

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+export const App = () => {
+  
+
+
+
+  return (
+    <table>
+      <th>
+        <td>Nombre</td>
+        <td>Genero</td>
+        <td>Longitud</td>
+        <td>Latitud</td>
+        <td>Correo</td>
+        <td>Fecha de Nacimiento</td>
+        <td>Imagen</td>
+      </th>
+      <tbody>
+        <td>{}</td>
+      </tbody>
+    </table>
+  )
+}
